da27342a6d81653b68cd6248cf36673cbe1fabdb
calebhoang <calebhoangm@gmail.com>
2025-12-01 17:48:47 -0600

    Added full scoring + set functionality

--- rps.js
+++ rps.js
@@ -1,13 +1,17 @@
-const txtbox = document.querySelector("status")
-const btnRock = document.querySelector("rock")
-const btnPaper = document.querySelector("paper")
-const btnScissors = document.querySelector("scissors")
+const txtbox = document.querySelector("#status")
+const results = document.querySelector("#results")
+const btnRock = document.querySelector("#rock")
+const btnPaper = document.querySelector("#paper")
+const btnScissors = document.querySelector("#scissors")
 
+
+let pscore = 0 // player score
+let cscore = 0 // computer score
 
 // Get the player's input based on the button clicked.
-btnRock.addEventListener("click", judgeRound(0)) // ROCK
-btnPaper.addEventListener("click", judgeRound(1)) // PAPER
-btnScissors.addEventListener("click", judgeRound(2)) // SCISSORS
+btnRock.addEventListener("click", function(){playRound(0)}) // ROCK
+btnPaper.addEventListener("click", function(){playRound(1)}) // PAPER
+btnScissors.addEventListener("click", function(){playRound(2)}) // SCISSORS
 
 
 // Get the computer move, but do not reveal it to the player.
@@ -16,41 +20,74 @@ function computerMove() {
     let randVal = Math.random();
 
     if (randVal <= 1/3) {
-        console.log("Computer chose ROCK.")
+        txtbox.textContent="Computer chose ROCK."
         return 0;
     } else if (randVal <= 2/3) {
-        console.log("Computer chose PAPER.")
+        txtbox.textContent="Computer chose PAPER."
         return 1;
     } else {
-        console.log("Computer chose SCISSORS.")
+        txtbox.textContent="Computer chose SCISSORS."
         return 2;
     }
 }
 
+function playRound(player) {
+    if (cscore + pscore < 5) {
 
-// 
+        // play a round
+        let point = judgeRound(player)
+        let resultString = ""
+        if (point == 0) {
+            resultString = resultString + "Tie! No points awarded."
+        } else if (point == 1) {
+            resultString = resultString +  "Player wins!"
+            pscore ++
+        } else if (point == -1) {
+            resultString = resultString +  "Computer wins!"
+            cscore ++
+        }
+
+        // display score
+
+        resultString = resultString +  " Score: (" + pscore + "-" + cscore + ")."
+
+        if (pscore + cscore == 5) {
+            if (pscore == 3) {
+                resultString = resultString + " Player wins the set!"
+            }
+
+            if (cscore == 3) {
+                resultString = resultString + " Computer wins the set!"
+            }
+        }
+
+
+
+        results.textContent = resultString + ""
+        console.log(resultString)
+    }
+    
+}
+
+
+// If the game has not yet ended, play out a round using the player's choice.
 function judgeRound(player) {
     const computer = parseInt(computerMove())
     player = parseInt(player)
     if (computer == player) {
-        console.log("Tie! No points awarded.")
         return 0;
     }
 
     if (player < 2) {
         if (computer == player + 1) {
-            console.log("Computer wins!")
             return -1;
         } else {
-            console.log("Player wins!")
             return 1;
         }
     } else {
         if (computer == 0) {
-            console.log("Computer wins!")
             return -1;
         } else {
-            console.log("Player wins!")
             return 1;
         }
     }
